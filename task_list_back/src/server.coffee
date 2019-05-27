35c0c5d7e497414af204cd41a91ad806b5aa19a3
ptrees <ptrees@126.com>
2019-05-27 17:38:40 +0800

    finish!

--- task_list_back/src/server.coffee
+++
@@ -1,5 +0,0 @@
-ex = require 'express'
-app = ex()
-
-app.get('/', (req,res) =>res.send("Hello"))
-app.listen(3000, ()=>console.log("Example log."))
--- task_list_back/src/server.js
+++ task_list_back/src/server.js
@@ -2,9 +2,26 @@ const ex = require('express')
 const xlsx = require('node-xlsx')
 var xlsx_file_path = "./task_list.xlsx"
 
-//var task_list = xlsx.parse(xlsx_file_path)
-
 const app = ex()
-app.get("/xlsx_data", (req,res) => res.send(JSON.stringify(xlsx.parse(xlsx_file_path))))
+
+getContent = function () { 
+  var content = xlsx.parse(xlsx_file_path)
+  var content1 = []
+  tmplist = []
+  for (y in [...Array(content[0].data.length)]){
+    content1[y]=[]
+    for(x in [...Array(content.length)]){
+      content1[y].push(content[x].data[y][0])
+    }
+  }
+  content1.unshift([])
+  for (i in [...Array(content.length)]){
+    content1[0].push(content[i].name)
+    //content1[0].push(item.name)
+  }
+  return content1
+ }
+
+app.get("/xlsx_data", (req,res) => res.send(JSON.stringify(getContent())))
 
 app.listen(3000, ()=>{console.log("Running")})
